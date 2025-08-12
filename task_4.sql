4e4f29b6881ef47e03efe6b9ba84b72f5e4557b9
Damen202 <babatundeogundare195@gmail.com>
2025-08-12 04:21:55 +0100

    Full Table Description

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,6 @@
--- Print the full description of the books table without DESCRIBE or EXPLAIN
-SHOW CREATE TABLE books;
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';

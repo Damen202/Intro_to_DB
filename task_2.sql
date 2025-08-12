5d9805adfaf51473b0298d65c7570b7379289aaa
Damen202 <babatundeogundare195@gmail.com>
2025-08-12 04:14:29 +0100

    Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -1,14 +1,14 @@
 -- Switch to the alx_book_store database
 USE alx_book_store;
 
--- AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS AUTHORS (
+-- Authors TABLE
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- BOOKS TABLE
-CREATE TABLE IF NOT EXISTS BOOKS (
+-- Books TABLE
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
